600491b4ea3360fc4de5bb636bdaeab8c4fc6beb
Auspicious-1ife <melissagushi50@gmail.com>
2024-04-17 15:05:49 +0000

    After series of headaches I have been able to use the vagrant box on my pc and then Writing a function that returns the list of available attributes and methods of an object

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,14 +1,3 @@
-#!/usr/bin/python3
 def lookup(obj):
-# Get the list of all attributes and methods of the object
-all_attrs = dir(obj)
-# Initialize an empty list to store public attributes and methods
-public_attrs = []
-# Iterate through all attributes/methods
-for attr in all_attrs:
-# Check if the attribute/method is not private (doesn't start with double underscore)
-if not attr.startswith("__"):
-# Add the attribute/method to the list of public attributes/methods
-public_attrs.append(attr)
-# Return the list of public attributes/methods
-return (public_attrs)
+    """Return the list of available attributes and methods of an object."""
+    return dir(obj)
