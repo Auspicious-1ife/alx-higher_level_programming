2c889cfc886e80b2af3b26c0427a7d04f4cf1bd1
Auspicious-1ife <melissagushi50@gmail.com>
2024-04-18 15:51:43 +0000

    testing my codes for task 2 which is, to a function that prints My name is <first name> <last name>

---
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -0,0 +1,20 @@
+def say_my_name(first_name, last_name=""):
+    """
+    Prints "My name is <first name> <last name>"
+
+    Args:
+        first_name (str): The first name.
+        last_name (str): The last name. Defaults to an empty string.
+
+    Raises:
+        TypeError: If first_name or last_name are not strings.
+
+    """
+    if type(first_name) is not str:
+        raise TypeError("first_name must be a string")
+    if type(last_name) is not str:
+        raise TypeError("last_name must be a string")
+    if last_name:
+        print("My name is {} {}".format(first_name, last_name))
+    else:
+        print("My name is {}".format(first_name))
