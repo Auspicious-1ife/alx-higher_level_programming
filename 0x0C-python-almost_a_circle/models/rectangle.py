fd34be32ebe06ab19874b46f62473bb7aea3576e
Auspicious-1ife <melissagushi50@gmail.com>
2024-04-24 15:15:31 +0000

    Working on task2  .

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -1,11 +1,13 @@
 #!/usr/bin/python3
 """Rectangle module"""
 
+
 from models.base import Base
 
+
 class Rectangle(Base):
+
     """Rectangle class"""
-    
     def __init__(self, width, height, x=0, y=0, id=None):
         """Constructor method"""
         super().__init__(id)
