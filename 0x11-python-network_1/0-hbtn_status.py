e86e3112582c2f5ad9c0f2f8750ec6dbc0c13cff
Auspicious-1ife <melissagushi50@gmail.com>
2024-07-05 16:18:38 +0100

    a python script that fetches https://alx-intranet.hbtn.io/status

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+This module provides functionality to fetch and display the status from a URL.
+"""
+
+import urllib.request
+
+def fetch_status(url):
+    """Fetches and prints the status from the specified URL."""
+    with urllib.request.urlopen(url) as response:
+        body = response.read()
+        decoded_body = body.decode('utf-8')
+    print("Body response:")
+    print("\t- type: {}".format(type(body)))
+    print("\t- content: {}".format(body))
+    print("\t- utf8 content: {}".format(decoded_body))
+if __name__ == "__main__":
+    url = 'https://alx-intranet.hbtn.io/status'
+    fetch_status(url)
