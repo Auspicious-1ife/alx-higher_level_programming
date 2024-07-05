387f4f97b2e40d842313213c8ad324919f9c08fd
Auspicious-1ife <melissagushi50@gmail.com>
2024-07-05 19:04:23 +0100

    corrected the line 16 to produce correct output

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-This script fetches https://alx-intranet.hbtn.io/status
+This script fetches https://intranet.hbtn.io/status
 using the requests package
 and displays the body of the
 response in the required format.
@@ -10,15 +10,14 @@ import requests
 
 
 def fetch_status():
-    """Fetches and displays the
-    status from https://alx-intranet.hbtn.io/status."""
-    url = 'https://alx-intranet.hbtn.io/status'
+    """Fetches and displays the status from https://intranet.hbtn.io/status."""
+    url = 'https://intranet.hbtn.io/status'
     response = requests.get(url)
-    data = response.json()  # Assuming the response content is JSON
+    content = response.text.strip()
 
     print("Body response:")
-    print("\t- type: {}".format(type(data)))
-    print("\t- content: {}".format(data))
+    print("\t- type: {}".format(type(content)))
+    print("\t- content: {}".format(content))
 
 
 if __name__ == "__main__":
