b991e04f54a11842190978be2bbccaaf51114a41
Auspicious-1ife <melissagushi50@gmail.com>
2024-07-05 16:27:20 +0100

    edited the pycodestyle errors

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -5,6 +5,7 @@ This module provides functionality to fetch and display the status from a URL.
 
 import urllib.request
 
+
 def fetch_status(url):
     """Fetches and prints the status from the specified URL."""
     with urllib.request.urlopen(url) as response:
@@ -14,6 +15,7 @@ def fetch_status(url):
     print("\t- type: {}".format(type(body)))
     print("\t- content: {}".format(body))
     print("\t- utf8 content: {}".format(decoded_body))
-if __name__ == "__main__":
-    url = 'https://alx-intranet.hbtn.io/status'
+
+    if __name__ == "__main__":
+        url = 'https://alx-intranet.hbtn.io/status'
     fetch_status(url)
