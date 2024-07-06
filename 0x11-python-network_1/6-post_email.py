1348719194d100ae24d5352ac3499c37a3d3cd7c
Auspicious-1ife <melissagushi50@gmail.com>
2024-07-06 16:21:38 +0100

    Writing a Python script that takes in a URL and an email address, sends a POST request to the passed URL with the email as a parameter

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -1,25 +1,25 @@
 #!/usr/bin/python3
-"""Write a Python script that
-takes in a URL and an email
-address, sends a POST request to
-the passed URL with the email as
-a parameter, and finally displays
-the body of the response."""
+"""
+A script that takes in a URL and an email address,
+sends a POST request to the passed URL
+with the email as a parameter, and displays the body of
+the response.
 
+Usage:
+    ./6-post_email.py <URL> <email>
+"""
 import requests
 import sys
 
-
-# Get the URL and email from command-line arguments
 if __name__ == "__main__":
     url = sys.argv[1]
     email = sys.argv[2]
 
-# Prepare the payload
-payload = {'email': email}
+    # Prepare the payload
+    payload = {'email': email}
 
-# Send the POST request
-response = requests.post(url, data=payload)
+    # Send the POST request
+    response = requests.post(url, data=payload)
 
-# Display the body of the response
-print(response.text)
+    # Display the body of the response
+    print(response.text)
