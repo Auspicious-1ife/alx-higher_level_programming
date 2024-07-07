b889d0b25b7439bdf91f0619394d5374e48d6f65
Auspicious-1ife <melissagushi50@gmail.com>
2024-07-07 12:33:17 +0100

    updated

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -11,19 +11,14 @@ import requests
 import sys
 
 
-def fetch_request_id(url):
-    """Fetches the value of
-    X-Request-Id from the
-    response header."""
-    response = requests.get(url)
-    request_id = response.headers.get('X-Request-Id')
-
-    if request_id:
-        print(request_id)
-    else:
-        print("No X-Request-Id found in the response headers.")
-
-
 if __name__ == "__main__":
     url = sys.argv[1]
-    fetch_request_id(url)
+    email = sys.argv[2]
+# Prepare the payload
+    payload = {'email': email}
+
+    # Send the POST request
+    response = requests.post(url, data=payload)
+
+    # Display the body of the response
+    print(response.text)
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-My python project on Alx
+# ALX Higher Level Programming
+This repository contains various Python projects completed as part of the ALX Higher Level Programming curriculum. Each project focuses on different aspects of Python programming, from basic syntax and operations to more advanced topics like networking and web scraping. It also contains some basics on javascript for warm up and object scopes and scores
+
