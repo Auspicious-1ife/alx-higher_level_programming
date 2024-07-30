73b6b5059a410b056f703744a05c6e6cca369eab
Auspicious-1ife <melissagushi50@gmail.com>
2024-07-30 13:26:08 +0100

    a  script that writes a string to a file.

---
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -0,0 +1,21 @@
+#!/usr/bin/node
+
+const fs = require('fs');
+
+// Gets the file path and string to write from command line arguments
+const filePath = process.argv[2];
+const stringToWrite = process.argv[3];
+
+// Checks if both arguments are provided
+if (!filePath || !stringToWrite) {
+  console.error('Usage: ./1-writeme.js <file_path> <string_to_write>');
+  process.exit(1);
+}
+
+// Writes the string to the file in utf-8 encoding
+fs.writeFile(filePath, stringToWrite, 'utf-8', (err) => {
+  if (err) {
+    // Prints the error object if an error occurred during writing
+    console.error(err);
+  }
+});
